f6a6be04b1cd1fad9f302d07052406624e93ad4d
Rohan Venkatapuram <sharpedo75@gmail.com>
2022-01-16 21:03:40 -0800

    more changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import TopBar from "./Components/TopBar";
 import {useRef, useState} from "react";
-import Unity, { UnityContext } from "react-unity-webgl";
+import Unity, {UnityContext} from "react-unity-webgl";
 
 function App() {
 
+    const gameRef = useRef(null)
+    const conceptRef = useRef(null)
+    const downloadRef = useRef(null)
 
-    // const specialRef = useRef(null)
-    //
-    // function scrollTo() {
-    //     console.log("hello")
-    //     specialRef.current.scrollIntoView()
-    // }
+
+    function toGame() {
+        gameRef.current.scrollIntoView()
+    }
+
+    function toConcept() {
+        conceptRef.current.scrollIntoView()
+    }
+
+    function toDownloads() {
+        console.log("hello")
+        downloadRef.current.scrollIntoView()
+    }
+
+    let scrollFunctions = [toGame, toConcept, toDownloads]
 
     const unityContext = new UnityContext({
         loaderUrl: "/workshop/Build/WebGL.loader.js",
@@ -19,37 +31,55 @@ function App() {
         codeUrl: "/workshop/Build/WebGL.wasm",
     });
 
-    function activateFullscreen() {
-        unityContext.setFullscreen(true);
-    }
-
     const [sidebar, setSidebar] = useState(false)
+
+    const gameViewWidth = 50
+    const gameViewHeight = gameViewWidth / 16 * 9
 
     let margin = sidebar ? "ml-48" : "ml:0"
 
+
+    const conceptArtPath = "/workshop/conceptArt/"
+    let conceptArts = ["crewmate.png", "imposter.jfif", "map.jfif"]
+    let conceptDescriptions = ["Main Character Design", "Enemy Design", "Environment Design"]
+
     return (
-        <div >
-            <TopBar sidebar={sidebar} setSidebar={setSidebar}/>
+        <div>
+            <TopBar sidebar={sidebar} setSidebar={setSidebar} scrollFunctions={scrollFunctions}/>
             <div class={"transition-all " + margin}>
                 <div class="h-16"/>
                 <div class="flex flex-col justify-center items-center">
                     {/*Game section*/}
-                    <span class="mt-5 font-bold text-5xl mb-5">Game</span>
+                    <span ref={gameRef} class="mt-5 font-bold text-5xl mb-5">Game</span>
                     <Unity
                         unityContext={unityContext}
                         style={{
-                            width: "640px",
-                            height: "360px",
+                            width: gameViewWidth + "vw",
+                            height: gameViewHeight + "vw",
                         }}
                     />
-                    <button class="
-                    mt-5 rounded-full border border-black p-5 bg-white text-black transition-all
-                    hover:border-white hover:bg-black hover:text-white"
-                            onClick={activateFullscreen}
-                    >Enter Fullscreen</button>
                     <div class="w-4/5 mt-10 border-b border-gray-400"/>
+                    {/*Concept Art*/}
+                    <span ref={conceptRef} class="mt-10 font-bold text-5xl mb-5">Concept Art</span>
+                    <div class="m-10">
+                        <div className="grid grid-cols-3 grid-flow-row w-full gap-5 justify-center items-center">
+                            {conceptArts.map((art, i) => {
+                                return (
+                                    <div
+                                        class="flex justify-left items-center flex-col rounded border border-gray-300 w-full h-96">
+                                        <img src={conceptArtPath + art} class="w-auto h-4/5 p-5"/>
+                                        <div
+                                            class="p-5 text-2xl border-t w-full border-t-gray-300 text-black text-center">
+                                            {conceptDescriptions[i]}
+                                        </div>
+                                    </div>)
+                            })}
+
+                        </div>
+                    </div>
+                    <div className="w-4/5 mt-10 border-b border-gray-400"/>
                     {/*Download Page*/}
-                    <span className="mt-10 font-bold text-5xl mb-5">Downloads</span>
+                    <span ref={downloadRef} className="mt-10 font-bold text-5xl mb-5">Downloads</span>
                     <a href="/workshop/Windows Build.zip"
                        class="mt-5 rounded-full bg-black text-white p-5 flex flex-row items-center justify-center"
                        download>
--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -15,11 +15,18 @@ function TopBar(props) {
     let topBarClass = "h-16 w-full fixed flex flex-row items-center justify-left bg-gradient-to-r from-black to-gray-600 transition-all" +
         (props.sidebar ? " ml-48" : "")
 
-    let sideBarClass = "fixed flex flex-col justify-center h-full bg-black transition-all bg-gradient-to-b from-black to-gray-600 " +
+    let sideBarClass = "fixed flex flex-col justify-left h-full bg-black transition-all bg-gradient-to-b from-black to-gray-600 " +
         (props.sidebar ? "w-48" : "w-0")
 
     let icon = props.sidebar ? faTimes : faBars;
+    // <div className="mt-5 text-white text-2xl p-5 border-white border-b">
+    //     Game
+    // </div>
 
+    let sidebarContent = [];
+    if (props.sidebar) {
+        sidebarContent = ["Game", "Concept Art", "Downloads"]
+    }
 
     return (
         <div>
@@ -31,10 +38,18 @@ function TopBar(props) {
                 >
                     <FontAwesomeIcon icon={icon}/>
                 </button>
-                <div class="pl-5 text-white text-2xl">Workshop Game</div>
+                <div class="pl-5 text-white text-2xl">Workshop Example</div>
             </div>
             <div class={sideBarClass}>
-
+                {sidebarContent.map((value, index) => {
+                    return (
+                        <button className="mt-5 text-white text-2xl p-5 border-white border-b"
+                                onClick={props.scrollFunctions[index]}
+                        >
+                            {value}
+                        </button>
+                    )
+                })}
             </div>
         </div>
     );
